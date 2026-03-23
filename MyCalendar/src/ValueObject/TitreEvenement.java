1e2d0644d09796b80c6f560ef35e93d2e51bd682
Geoffrey2501 <132345682+Geoffrey2501@users.noreply.github.com>
2026-03-23 09:09:58 +0100

    Ajout TitreEvenement + test :white_check_mark:

---
+++ MyCalendar/src/ValueObject/TitreEvenement.java
@@ -0,0 +1,9 @@
+package ValueObject;
+
+public record TitreEvenement(String valeur) {
+    public TitreEvenement {
+        if (valeur == null || valeur.isBlank()) {
+            throw new IllegalArgumentException("Le titre ne peut pas être vide");
+        }
+    }
+}
