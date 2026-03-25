65c25186a702869d44ba2829cebb250a9c2ae6f0
Geoffrey2501 <132345682+Geoffrey2501@users.noreply.github.com>
2026-03-25 09:35:39 +0100

    Ajout de deux méthodes dans dateEvenement ✅

--- MyCalendar/src/ValueObject/DateEvenement.java
+++ MyCalendar/src/ValueObject/DateEvenement.java
@@ -6,4 +6,12 @@ public record DateEvenement(LocalDateTime valeur) {
     public DateEvenement {
         if (valeur == null) throw new IllegalArgumentException("La date ne peut pas être null");
     }
+
+    public boolean estAvant(DateEvenement autre) {
+        return valeur.isBefore(autre.valeur);
+    }
+
+    public DateEvenement getDateFin(DureeMinutes dureeMinutes) {
+        return new DateEvenement(valeur.plusMinutes(dureeMinutes.valeur()));
+    }
 }
